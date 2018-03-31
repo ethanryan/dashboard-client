367829cf60f002d4258e7580cfe3a9e680ca5801
ethanryan <ethanryan@gmail.com>
2018-03-31 16:29:43 -0400

    updated ShowUsers

---
+++ src/components/ShowUsers.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ShowUsers = (props) => {
+
+  console.log('hello from ShowUsers')
+  return(
+
+    <div key={props.id} className='center'>
+      ShowUsers will do here!!!
+    </div>
+  )
+}
+
+export default ShowUsers
