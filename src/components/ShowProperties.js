a93c16e4db4fede69b3c583e69e1a2e4ac081572
ethanryan <ethanryan@gmail.com>
2018-04-02 10:01:31 -0400

    added window info to ShowProperties

--- src/components/ShowProperties.js
+++ src/components/ShowProperties.js
@@ -14,7 +14,51 @@ const ShowProperties = (props) => {
           ?
           props.properties.map( (property) => {
             if ( props.userBuildings.includes(property.name) ) {
-              return (`${property.name}`)
+              console.log('property is: ', property)
+              return (
+                <div key={property.id} className='each-property'>
+                  <h3>
+                    {property.name}
+                  </h3>
+                  {property.address}
+
+                  <div>
+                    Windows: {property.windows.length}
+                    <div>
+                      {property.windows.map( (eachWindow) => {
+                        return (
+                          <div key={eachWindow.id} className='each-window'>
+                            <div>
+                            <h4>
+                              Window info:
+                            </h4>
+                            <div>Hourly foot traffic: {eachWindow.hourly_foot_traffic}</div>
+                            <div>Hourly mobile devices: {eachWindow.hourly_mobile_devices}</div>
+                            <div>Hourly vehicle traffic: {eachWindow.hourly_vehicle_traffic}</div>
+                          </div>
+                        </div>
+                        )
+                      })}
+                 </div>
+                 </div>
+
+                  <div>
+                    Ads: {property.ads.length}
+                    <div>
+                      {property.ads.map( (ad) => {
+                        return (
+                          <div key={ad.id} className='each-ad'>
+                            <h4>
+                              Ad campaign: {ad.name}
+                            </h4>
+                            Ad impressions: {ad.hourly_impressions}
+                          </div>
+                        )
+                      })}
+                    </div>
+                  </div>
+                </div>
+              )
             }
             return "*****"
           })
