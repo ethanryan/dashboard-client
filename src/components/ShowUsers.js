e6c046ddd029dc4b7a96050c777b912fc4e41aa9
ethanryan <ethanryan@gmail.com>
2018-04-02 23:34:18 -0400

    updating styling

--- src/components/ShowUsers.js
+++ src/components/ShowUsers.js
@@ -23,6 +23,7 @@ const ShowUsers = (props) => {
   return(
 
     <div key={props.id} className='center show-users'>
+      <Card fluid>
       ShowUsers will be here!!!
 
       <br></br>
@@ -31,66 +32,82 @@ const ShowUsers = (props) => {
       {allUsers.length > 0 ? allUsers.map( (user) => {
         return (
           <div key={user.id} className='each-user'>
-            <h1>
-              {`${user.name}`}
-            </h1>
-            <div>
-              {user.name === "Trump" ? "THIS IS TRUMP!!!" : "nope"}
+            <Card fluid color='blue'>
+
+              <Card.Content>
+
+
+                <Card.Header>
+                  <h1>
+                    {user.name}
+                  </h1>
+                  {`user id: ${user.id}`}
+                </Card.Header>
+
+                {/* <div>
+                  {user.name === "Trump" ? "THIS IS TRUMP!!!" : "nope"}
+                </div> */}
+
+                <Card.Description>
+                  {`"${user.bio}"`}
+                </Card.Description>
+              </Card.Content>
+
+
+              <Card.Content>
+
+              <div>
+                {`Buildings: ${user.buildings.length}`}
+              </div>
+
+              {user.buildings.length > 0 ?
+                //const filteredStories = props.stories.filter(story => story.user.id === props.user.id)
+
+                //const matchingProperty = allProperties.filter(property => property.name === building.name)
+                <ShowProperties
+                  // properties={allProperties.map(property => property.name === user.buildings.filter(building => building.name === property.name) )}
+                  properties={allProperties}
+                  userBuildings={user.buildings.map(building => building.name)}
+                />
+                // user.buildings.map( (building) => {
+                //   const matchingProperty = allProperties.filter(property => property.name === building.name)
+                //   console.log('matchingProperty is: ', matchingProperty)
+                //
+                //   return (
+                //     <div key={building.id} className='each-building'>
+                //       <h3>
+                //         {building.name}
+                //       </h3>
+                //       {building.address}
+                //       <br></br>
+                //       {allProperties.length > 0 ? allProperties.map( (property) => {
+                //
+                //         //const filteredStories = props.stories.filter(story => story.user.id === props.user.id)
+                //
+                //         // const matchingProperty = allProperties.filter(property => property.name === building.name)
+                //         // console.log('matchingProperty is: ', matchingProperty)
+                //
+                //         return (
+                //           <div key={property.id} className='each-property'>
+                //               {matchingProperty.length > 0 ?
+                //               `matching property is: ${matchingProperty[0].name}`
+                //               :
+                //               "no matching property"}
+                //           </div>
+                //         )
+                //       })
+                :
+                "no properties"}
+
+              </Card.Content>
+              </Card>
             </div>
-            <h2>
-              {`user id: ${user.id}`}
-              <br></br>
-              {`"${user.bio}"`}
-            </h2>
-            <div>
-              {`Buildings: ${user.buildings.length}`}
-            </div>
-
-            {user.buildings.length > 0 ?
-              //const filteredStories = props.stories.filter(story => story.user.id === props.user.id)
-
-              //const matchingProperty = allProperties.filter(property => property.name === building.name)
-              <ShowProperties
-                // properties={allProperties.map(property => property.name === user.buildings.filter(building => building.name === property.name) )}
-                properties={allProperties}
-                userBuildings={user.buildings.map(building => building.name)}
-              />
-              // user.buildings.map( (building) => {
-              //   const matchingProperty = allProperties.filter(property => property.name === building.name)
-              //   console.log('matchingProperty is: ', matchingProperty)
-              //
-              //   return (
-              //     <div key={building.id} className='each-building'>
-              //       <h3>
-              //         {building.name}
-              //       </h3>
-              //       {building.address}
-              //       <br></br>
-              //       {allProperties.length > 0 ? allProperties.map( (property) => {
-              //
-              //         //const filteredStories = props.stories.filter(story => story.user.id === props.user.id)
-              //
-              //         // const matchingProperty = allProperties.filter(property => property.name === building.name)
-              //         // console.log('matchingProperty is: ', matchingProperty)
-              //
-              //         return (
-              //           <div key={property.id} className='each-property'>
-              //               {matchingProperty.length > 0 ?
-              //               `matching property is: ${matchingProperty[0].name}`
-              //               :
-              //               "no matching property"}
-              //           </div>
-              //         )
-              //       })
-                    :
-                    "no properties"}
-
-          </div>
-        )
-      }
+          )
+        }
       )
-       : "allUsers here"}
+      : "allUsers here"}
 
+    </Card>
     </div>
   )
 }
