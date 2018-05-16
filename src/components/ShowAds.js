40525857628d2af0ac77961b2128acc7d1f49b69
ethanryan <ethanryan@gmail.com>
2018-05-16 00:28:47 -0400

    added LineChart to ShowAds

--- src/components/ShowAds.js
+++ src/components/ShowAds.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 import { Card, Statistic } from 'semantic-ui-react'
 
-import BusinessChart from '../assets/BusinessChart.svg'
+// import ReactChartkick, { LineChart, PieChart } from 'react-chartkick'
+import ReactChartkick, { LineChart } from 'react-chartkick'
+import Chart from 'chart.js'
+
+// import BusinessChart from '../assets/BusinessChart.svg'
+
+ReactChartkick.addAdapter(Chart)
+
 
 const ShowAds = (props) => {
 
@@ -25,6 +32,19 @@ const ShowAds = (props) => {
                     {property.ads ? property.ads.map( (ad, index) => {
                       return (
                         <div key={index} className='each-ad-in-advertisment'>
+
+                          {/* <LineChart data={[[new Date(), 5], [1368174456, 4], ["2017-01-01 00:00:00 UTC", 7]]} /> */}
+
+                          <LineChart data={[
+                            [new Date(new Date().setDate(new Date().getDate()-42)), (ad.hourly_impressions / 3)],
+                            [new Date(new Date().setDate(new Date().getDate()-35)), (ad.hourly_impressions - 75)],
+                            [new Date(new Date().setDate(new Date().getDate()-28)), (ad.hourly_impressions - 150)],
+                            [new Date(new Date().setDate(new Date().getDate()-21)), (ad.hourly_impressions / 2)],
+                            [new Date(new Date().setDate(new Date().getDate()-14)), (ad.hourly_impressions + 100)],
+                            [new Date(new Date().setDate(new Date().getDate()-7)), ad.hourly_impressions - 175],
+                            [new Date(), ad.hourly_impressions]
+                          ]} />
+
                           <h4>
                             Ad campaign:
                           </h4>
@@ -44,9 +64,9 @@ const ShowAds = (props) => {
 
                   <br></br>
 
-                  <div className="pie-chart-img">
+                  {/* <div className="pie-chart-img">
                     <img src={BusinessChart} alt="business chart"/>
-                  </div>
+                  </div> */}
 
                   <Card.Header className='show-properties-card-header'>
                     <h2>
