460ea11059f5a6680c24fd0361bffcbe848e8b0f
ethanryan <ethanryan@gmail.com>
2018-05-17 00:11:24 -0400

    updated DashBoardContainer, styling tabs

--- src/container/DashboardContainer.js
+++ src/container/DashboardContainer.js
@@ -34,35 +34,61 @@ class DashboardContainer extends Component {
 
   render() {
     console.log('-------->>>> 0. state from DashboardContainer: ', this.state)
+    // console.log('-----this user is: ', this.state.users.filter(user => user.buildings.length < 1)[0])
+
+    const advertisers = this.state.users.filter(user => user.buildings.length < 1) //an array
+    // console.log('-----advertisers is: ', advertisers)
 
     const panes = [
       { menuItem: `All Users - ${this.state.users.length}`, render: () =>
       <Tab.Pane>
+        <br></br>
+        <h3>
+          All Users
+        </h3>
         <ShowUsers
           users={this.state.users}
           properties={this.state.properties}
         />
       </Tab.Pane>
-    },
+      },
 
-    { menuItem: `Property Owners - ${this.state.users.filter(user => user.buildings.length > 0).length}`, render: () =>
-    <Tab.Pane>
-      <ShowUsers
-        users={this.state.users.filter(user => user.buildings.length > 0)}
-        properties={this.state.properties}
-      />
-    </Tab.Pane>
-  },
+      { menuItem: `Property Owners - ${this.state.users.filter(user => user.buildings.length > 0).length}`, render: () =>
+      <Tab.Pane>
+        <ShowUsers
+          users={this.state.users.filter(user => user.buildings.length > 0)}
+          properties={this.state.properties}
+        />
+      </Tab.Pane>
+      },
 
-  { menuItem: `Advertisers - ${this.state.users.filter(user => user.buildings.length < 1).length}`, render: () =>
-  <Tab.Pane>
-    <ShowUsers
-      users={this.state.users.filter(user => user.buildings.length < 1)}
-      properties={this.state.properties}
-    />
-  </Tab.Pane>
-},
-]
+      { menuItem: `Advertisers - ${this.state.users.filter(user => user.buildings.length < 1).length}`, render: () =>
+      <Tab.Pane>
+        <ShowUsers
+          users={this.state.users.filter(user => user.buildings.length < 1)}
+          properties={this.state.properties}
+        />
+      </Tab.Pane>
+      },
+
+      // { menuItem: `${advertisers.length > 1 ? advertisers[0].name : 'null'}`, render: () =>
+      // <Tab.Pane>
+      //   <ShowUsers
+      //     users={advertisers.length > 1 ? advertisers[0] : 'null'}
+      //     properties={this.state.properties}
+      //   />
+      // </Tab.Pane>
+      // },
+      //
+      // { menuItem: `${advertisers.length > 1 ? advertisers[1].name : 'null'}`, render: () =>
+      // <Tab.Pane>
+      //   <ShowUsers
+      //     users={advertisers.length > 1 ? advertisers[1] : 'null'}
+      //     properties={this.state.properties}
+      //   />
+      // </Tab.Pane>
+      // },
+    ]
 
 return(
   <div key={this.props.id} className='center dashboard-tabs'>
